899f76866c91327c0b0f05a043e052005eeccdcc
CliffGreene <lifeguardtwenty@gmail.com>
2020-01-07 20:56:46 -0700

    Function to assert and compare arrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,21 @@
 const eqArrays = function(array1, array2) {
  
-  if (array1.length === array2.length) {
-    for (i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
+    if (array1.length === array2.length) {
+      for (i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i]) {
+          return false;
+        }
   
+      }
+      return true;
+    } else {
+      return false;
     }
-    return true;
-  } else {
-    return false;
-  }
-};
+  };
 
 const assertArraysEqual = function(asArray1, asArray2) {
-  const Answer = eqArrays(asArray1, asArray2);
-  if (Answer === true) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${asArray1} === ${asArray2}.`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${asArray1} !== ${asArray2}`);
-  }
-};
-assertArraysEqual([1,2,3], [1,2,4]);
+ const Answer = eqArrays(asArray1, asArray2);
+ if (Answer === true) {console.log(`🟢🟢🟢 Assertion Passed: ${asArray1} === ${asArray2}.`)}
+ else {console.log(`🔴🔴🔴 Assertion Failed: ${asArray1} !== ${asArray2}`)}}
+
   
