0a39d2d9dbc11cd0271b40b2b5ed6196c224517a
CliffGreene <lifeguardtwenty@gmail.com>
2020-01-11 12:33:27 -0700

    Attempted to add an inspect funtion to assertion checker: didn't work

--- eqObjects.js
+++ eqObjects.js
@@ -2,8 +2,9 @@
 //ASSERTION CHECKER
 
 const assertEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
   const Answer = actual;
-  if ((Answer === true) && (Answer === expected)) {
+  if (Answer === true)  {
     console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}.`);
   } else {
     console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
@@ -43,8 +44,6 @@ const eqObjects = function(Obj1,Obj2) {
   return true;
 };
 
-  
-/*
 const ab = { a: "1", b: [3] };
 const ba = { b: [3], a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
@@ -55,5 +54,4 @@ const cba = { c: "3", b: "2", a: "1" }
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd,cd2), false)
-*/
+assertEqual(ab,ba)
