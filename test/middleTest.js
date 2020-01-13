e1f7dca1f7827fc3deda88f37986e9e2743084a3
CliffGreene <lifeguardtwenty@gmail.com>
2020-01-13 23:31:05 +0000

    added mocha implementation to Head, and Tail

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,15 @@
-const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([3,5,2,6,8,6,4,2]), [6,8]);
-assertArraysEqual(middle([4,3,6]), [3]);
-assertArraysEqual(middle([8,5]), []);
+
+describe("#middle", () => {
+  it("returns [3] for [4, 3, 6]", () => {
+    assert.deepEqual(middle([4, 3, 6]), [3]);
+  });
+  it("returns [6, 8] for [10, 61, 6, 8, 13, 6]", () => {
+    assert.deepEqual(middle([10, 61, 6, 8, 13, 6]), [6, 8]);
+  });
+  it("returns [] for [1, 5]", () => {
+    assert.deepEqual(middle([1, 5]), []);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,12 @@
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require('chai').assert;
 
-assertEqual(tail(["foo", "bar"]), "bar");
+
+describe("#tail", () => {
+  it("returns 'bar' for ['foo', 'bar']", () => {
+    assert.strictEqual(tail(["foo", "bar"]), "bar");
+  });
+  it("returns 'baz' for ['cup', 'scint-ilating', 'baz']", () => {
+    assert.strictEqual(tail(['cup', 'scint-ilating', 'baz']), "baz");
+  });
+});
